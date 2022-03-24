5a3a71f0d9f0ea4ef27709d49e40c34e7868f525
jaweria961 <68546050+jaweria961@users.noreply.github.com>
2022-03-24 10:19:04 +0500

    Add files via upload

---
+++ Cocacola/index.js
@@ -0,0 +1,15 @@
+function imgSlider(anything){
+   document.querySelector('.cocacola').src = anything;
+}
+
+function changelogo(logo){
+    document.querySelector('.logo').src = logo;
+
+ }
+ 
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
+
